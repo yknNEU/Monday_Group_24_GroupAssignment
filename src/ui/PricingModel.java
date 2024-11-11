ef5556ca9adbc99ddd211b3f704c839952b4cb0a
YaoZhang-1120 <zhang.yao3@northeastern.edu>
2024-11-11 16:53:41 -0500

    t

--- src/ui/PricingModel.java
+++ src/ui/PricingModel.java
@@ -5,6 +5,8 @@ import model.Business.ConfigureABusiness;
 
 public class PricingModel {
 
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Business business = ConfigureABusiness.initialize();
